ecc2eadd1ebc51eec25a026c4bb4d14b09482513
Ahmad Jafari <71406620+sutedel@users.noreply.github.com>
2021-11-15 13:36:02 -0500

    master

---
+++ as4/src/app/new-releases/new-releases.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import data from '../data/NewReleasesAlbums.json';
+
+
+
+@Component({
+  selector: 'app-new-releases',
+  templateUrl: './new-releases.component.html',
+  styleUrls: ['./new-releases.component.css']
+})
+export class NewReleasesComponent implements OnInit {
+
+
+  
+  
+  releases:any;
+  constructor( ) { }
+
+
+  ngOnInit(): void {
+    this.releases= data.albums.items;
+  }
+  
+}
